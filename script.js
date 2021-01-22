3599fb5862fd908bcf3415db94de1241beab5dfe
khendersonPC <20666083+khendersonPC@users.noreply.github.com>
2021-01-22 15:07:54 -0700

    added images, temperature heading, animation (which isn't perfect)

--- script.js
+++ script.js
@@ -2,14 +2,19 @@
 // This is our API key
 var APIKey = "166a433c57516f51dfab1f7edaed8413";
 var city = "";
-
+var i=$("<img>");
+var tempDiv= $("<div>");
+var temp =$("<h2 class= 'tempH'>");
+$(".results").append(tempDiv);
+$(tempDiv).append(temp);
+$(".results").append(i);
 
 $("#current").click(function () {
     city = $("#city").val();
     alert("The city you selected is " + city);
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
         "q=" + city + "&appid=" + APIKey;
-
+        $("body").removeClass("yellow");
     // Here we run our AJAX call to the OpenWeatherMap API
     $.ajax({
         url: queryURL,
@@ -17,27 +22,46 @@ $("#current").click(function () {
     })
         // We store all of the retrieved data inside of an object called "response"
         .then(function (response) {
-
+            
             console.log(response.weather[0].main);
             console.log(response);
-
+            var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            $("h2").text("Current temperature: " + tempF.toFixed(1) + "&#x2103;");
             if(response.weather[0].main=="Thunderstorm" ||response.weather[0].main=="Rain" ||response.weather[0].main=="Drizzle"){
                 console.log("It is raining");
-                var i=$("<img src='clouds.png'>");
-                $(".results").append(i);
-                $("body").addClass("gray");
+                
+                
+                $("img").attr("src", "clouds_rain.png");
+
 
             }
             else if(response.weather[0].main=="Snow"){
+                console.log("It is snowing");
+                
+                $("img").attr("src", "snow.png");
+                
 
             }
             else if( response.weather[0].main=="Clear"){
-
+                console.log("It is sunny");
+                
+                $("img").attr("src", "sun.png");
+                $("body").addClass("yellow");
+                
+                for(var i = 0; i<50;i++){      
+                  $("img").animate({ height: "570px", width: "530px"} , 1500);
+                  $( "img" ).animate({height: "600px", width: "550px"}, 1500 );
+                }
             }
             else if(response.weather[0].main=="Clouds"){
+                console.log("It is partially cloudy");
+                
+                $("img").attr("src", "clouds_sun.png");
+                
 
             }
             else{
+               
                 console.log("You are experiencing extreme weather");
             }
 
@@ -71,3 +95,11 @@ $("#current").click(function () {
     // Here we are building the URL we need to query the database
 
 });
+
+var active = function(){
+    $('img').removeClass('inactive').addClass('active');
+  };
+  
+  var inactive = function(){
+    $('img').removeClass('active').addClass('inactive');
+  };
